6063ae314cb668ca4917ff81275ffa9443a30ffc
linchangmin <linchangmin@ubox.cn>
2016-11-06 17:09:07 +0800

    React中的属性props.children

--- app.jsx
+++ app.jsx
@@ -1,3 +1,18 @@
+class Comment extends React.Component{
+	render(){
+		return (
+			<div>
+				<div className="comment-body">
+					{this.props.children}
+				</div>
+				<div className="comment-author">
+					作者：{this.props.author}时间：{this.props.time}
+				</div>
+			</div>
+			)
+	}
+}
+
 class CommentForm extends React.Component{
 	render(){
 		return (
@@ -9,7 +24,17 @@ class CommentForm extends React.Component{
 class CommentList extends React.Component{
 	render(){
 		return (
-			<div className="comment-list">CommentList</div>
+			<div className="comment-list">
+				<Comment author="小李" time="2016-11-06">
+					沙发
+				</Comment>
+				<Comment author="小张" time="2016-11-05">
+					这是第2条评论
+				</Comment>
+				<Comment author="小林" time="2016-11-04">
+					这是第3条评论
+				</Comment>
+			</div>
 		)
 	}
 }
