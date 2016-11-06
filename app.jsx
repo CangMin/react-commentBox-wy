958fb94f93689a8a240c4a48ff8c3405a1025dc1
linchangmin <linchangmin@ubox.cn>
2016-11-06 22:01:22 +0800

    在componentDidMount生命周期中使用ajax异步请求json数据

--- app.jsx
+++ app.jsx
@@ -38,19 +38,43 @@ class CommentList extends React.Component{
 	}
 }
 
-var comments = [
-	{author:"小李",body:"这是一条评论",time:"20161106"},
-	{author:"小玲",body:"这是第二条评论",time:"20161105"},
-	{author:"小张",body:"这是第三条评论",time:"20161104"}
-];
+// var comments = [
+// 	{author:"小李",body:"这是一条评论",time:"20161106"},
+// 	{author:"小玲",body:"这是第二条评论",time:"20161105"},
+// 	{author:"小张",body:"这是第三条评论",time:"20161104"}
+// ];
 
 class CommentBox extends React.Component{
 
 	constructor(props){
 		super();
 		this.state = {
-			comments:props.comments//通过props将值传递给state,实现响应式编程。每当state值发生改变时，都会调用render()重新渲染Dom有变化的区域。
+			comments:props.comments || [],//通过props将值传递给state,实现响应式编程。每当state值发生改变时，都会调用render()重新渲染Dom有变化的区域。
 		};
+	}
+
+	loadDataFromServer(){
+		console.log("开始加载数据");
+		$.ajax({
+			url:"./comments.json",
+			dataType:"json",
+			success:function(comments){
+				// console.log(this);
+				console.log("服务端获取的数据"+comments);
+				this.setState({comments:comments});
+			}.bind(this),//让success回调函数中的this指向react对象
+			error: function (xhr) {
+                        console.log('动态页有问题！\n\n' + xhr.responseText);
+                    }
+		});
+			console.log("数据加载完成");
+	}
+
+	componentDidMount(){
+		console.log("组件渲染完成，下一步加载数据");
+		this.loadDataFromServer();
+		console.log("渲染完成");
+
 	}
 
 	render(){
@@ -65,7 +89,7 @@ class CommentBox extends React.Component{
 }
 
 ReactDOM.render(
-        <CommentBox comments={comments}/>,
+        <CommentBox/>,
         document.getElementById('content')
       );
 
